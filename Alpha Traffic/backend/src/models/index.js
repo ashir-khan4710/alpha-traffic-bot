524927cf9d60ef7af8056c08ae1d1bdf38c78325
ashir-khan4710 <ashir078915@gmail.com>
2026-06-13 15:05:02 +0500

    Delete Alpha Traffic directory

--- Alpha Traffic/backend/src/models/index.js
+++
@@ -1,8 +0,0 @@
-// Centralized model exports
-module.exports = {
-  User: require('./User'),
-  Profile: require('./Profile'),
-  Proxy: require('./Proxy'),
-  Log: require('./Log'),
-  Session: require('./Session')
-};
--- Alpha Traffic/backend/src/services/index.js
+++
@@ -1,7 +0,0 @@
-// Service layer exports
-module.exports = {
-  SessionManager: require('./sessionManager'),
-  ProxyManager: require('./proxyManager'),
-  BotEngine: require('./botEngine'),
-  SocketManager: require('./socketManager')
-};
